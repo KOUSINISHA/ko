86052bb4b408cc96c101ff3eaae7add921756741
KOUSINISHA <35996286+KOUSINISHA@users.noreply.github.com>
2018-03-07 15:48:41 +0530

    array.c
    good

---
+++ array.c
@@ -0,0 +1,11 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int=N,K;
+printf("Enter the value of 'N':");
+scanf("%d%d,'N':");
+printf("Enter the value of'K':");
+scanf("%d%d,'K':");
+getch()
+}
